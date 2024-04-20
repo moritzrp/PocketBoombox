a2ae51f68f51abd57a64d7d9465e5d1d2be7b10d
moritzrp <MoritzRp@proton.me>
2024-04-20 13:56:33 +0200

    Add project files.

---
+++ PocketBoombox/Patches/BoomboxPatch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FreeHandBoombox.Patches
+{
+    [HarmonyPatch(typeof(BoomboxItem))]
+    internal class BoomboxPatch
+    {
+        [HarmonyPatch("PocketItem")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            /*
+             * We have:
+             * [ ldarg.0, call, ldarg.0, ldc.i4.0, ldc.i4.0, call, ret ]
+             * 
+             * First call is base pocket method, second call turns off Boombox music
+             * 
+             * We want:
+             * [ ldarg.0, call, ret ]
+             */
+            List<CodeInstruction> allInstructions = new List<CodeInstruction>(instructions);
+
+            int startMusicArgIndex = allInstructions.FindLastIndex(instruction => instruction.IsLdarg());
+            int startMusicCallIndex = allInstructions.FindIndex(instruction => instruction.ToString().Contains("BoomboxItem::StartMusic"));
+
+            allInstructions.RemoveRange(startMusicArgIndex, startMusicCallIndex - startMusicArgIndex + 1);
+
+            return allInstructions.AsEnumerable();
+        }
+    }
+}
---
+++ PocketBoombox/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx.Logging;
+using BepInEx;
+using HarmonyLib;
+using FreeHandBoombox.Patches;
+
+namespace PocketBoombox
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class PocketBoomboxBase : BaseUnityPlugin
+    {
+        private const string modGUID = "moritzrp.PocketBoombox";
+        private const string modName = "PocketBoombox";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static PocketBoomboxBase instance;
+
+        internal ManualLogSource logger;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            harmony.PatchAll(typeof(PocketBoomboxBase));
+            harmony.PatchAll(typeof(BoomboxPatch));
+            logger.LogInfo("Boombox ready for pocketing");
+        }
+    }
+}
